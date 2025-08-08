5272f0b879706e0eec88da9aec8d6a5a224b8b19
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-08-08 23:57:15 +0500

    ci updated

--- .github/scripts/list-changes.sh
+++ .github/scripts/list-changes.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ "${{ github.event_name }}" = "pull_request" ]; then
-    CHANGED_FILES=$(git diff --name-only ${{ github.event.pull_request.base.sha }} ${{ github.sha }})
+EVENT_NAME=${1:-}
+BASE_SHA=${2:-}
+GITHUB_SHA=${3:-}
+
+if [ "$EVENT_NAME" = "pull_request" ]; then
+    CHANGED_FILES=$(git diff --name-only $BASE_SHA $GITHUB_SHA)
 else
     CHANGED_FILES=$(git diff --name-only HEAD~1 HEAD)
 fi
 
-echo "changes=$CHANGED_FILES" >> $GITHUB_OUTPUTs
+echo "changes=$CHANGED_FILES" >> $GITHUB_OUTPUT
