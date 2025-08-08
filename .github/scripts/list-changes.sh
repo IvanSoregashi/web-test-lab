d5e2c5ca4f5c0c06527f2da8d2fce4ce7167b523
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-08-09 02:16:09 +0500

    ci and python file

--- .github/scripts/list-changes.sh
+++ .github/scripts/list-changes.sh
@@ -28,7 +28,7 @@ fi
 
 if [ -n "$GITHUB_OUTPUT" ]; then
     echo "output changes to github output"
-    echo "changes=$CHANGED_FILES" >> "$GITHUB_OUTPUT"
+    echo "changes=$CHANGED_FILES" >> $GITHUB_OUTPUT
 else
     echo "GITHUB_OUTPUT is not set. THIS IS AN ISSUE"
     echo "changes=$CHANGED_FILES"
@@ -39,26 +39,26 @@ if [ -n "$PYTHON" ]; then
     echo "python changes detected"
     PYTHON_PROJECTS=$(git diff --name-only "$DIFF_BASE" "$DIFF_HEAD" -- 'python/*' | cut -d/ -f2 | sort -u)
     echo "python projects: $PYTHON_PROJECTS"
-    echo "python_projects=$PYTHON_PROJECTS" >> "$GITHUB_OUTPUT"
+    echo "python_projects=$PYTHON_PROJECTS" >> $GITHUB_OUTPUT
 fi
 JAVA=$(echo "$CHANGED_FILES" | grep "^java/" || true)
 if [ -n "$JAVA" ]; then
     echo "java changes detected"
     JAVA_PROJECTS=$(git diff --name-only "$DIFF_BASE" "$DIFF_HEAD" -- 'java/*' | cut -d/ -f2 | sort -u)
     echo "java projects: $JAVA_PROJECTS"
-    echo "java_projects=$JAVA_PROJECTS" >> "$GITHUB_OUTPUT"
+    echo "java_projects=$JAVA_PROJECTS" >> $GITHUB_OUTPUT
 fi
 JAVASCRIPT=$(echo "$CHANGED_FILES" | grep "^javascript/" || true)
 if [ -n "$JAVASCRIPT" ]; then
     echo "javascript changes detected"
     JAVASCRIPT_PROJECTS=$(git diff --name-only "$DIFF_BASE" "$DIFF_HEAD" -- 'javascript/*' | cut -d/ -f2 | sort -u)
     echo "javascript projects: $JAVASCRIPT_PROJECTS"
-    echo "javascript_projects=$JAVASCRIPT_PROJECTS" >> "$GITHUB_OUTPUT"
+    echo "javascript_projects=$JAVASCRIPT_PROJECTS" >> $GITHUB_OUTPUT
 fi
 OTHER=$(echo "$CHANGED_FILES" | grep -Ev '^(python|java|javascript)/' || true)
 if [ -n "$OTHER" ]; then
     echo "other changes detected"
-    echo "other_files=$OTHER" >> "$GITHUB_OUTPUT"
+    echo "other_files=$OTHER" >> $GITHUB_OUTPUT
 fi
 
 echo "python=$PYTHON"
